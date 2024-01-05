ccbb99b79fc0fbcfa9d4a7971f495661c077439e
lion48z <140863653+lion48z@users.noreply.github.com>
2024-01-04 21:42:12 -0600

    creating tables
    verifying that the information is what I want before I create the actual database

---
+++ server/workout_db.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE workout_tracker;
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    user_name VARCHAR(25) UNIQUE NOT NULL,
+    password VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE run (
+    run_id INT PRIMARY KEY AUTO_INCREMENT,
+    date DATE,
+    distance DECIMAL(10,2),
+    duration TIME,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+
+CREATE TABLE walk (
+    walk_id INT PRIMARY KEY AUTO_INCREMENT,
+    date DATE,
+    distance DECIMAL(10,2),
+    duration TIME,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE bike (
+    bike_id INT PRIMARY KEY AUTO_INCREMENT,
+    date DATE,
+    distance DECIMAL(10,2),
+    duration TIME,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
