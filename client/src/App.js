f8ae5c048b6cb3eb986ffded10682caee04ace00
lion48z <140863653+lion48z@users.noreply.github.com>
2024-01-10 14:09:06 -0600

    refactor with new components
    needs to have login and reg logic go way once in, is switching to Dashboard when logged in, need Logout logic added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link,  } from 'react-router-dom';
 import  Register  from './Register';
-import axios from 'axios';
+//import axios from 'axios';
 import Dashboard from './Dashboard';
+import Login from './Login';
 
 
 
-function App() {
-  
-  const [username, setUsername] = useState(''); 
-  const [password, setPassword] = useState('');
-  //create state variable to store auth token and logged in status
+function App() {   
   const [token, setToken] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   //const navigate = useNavigate();
@@ -19,71 +16,25 @@ function App() {
     console.log("client token: ", token);
   }, [token]);  
   
-  //handleLogin
-  const handleLogin = async () => {
-    //try logging in
-    try {
-      //send post request to /login route with the username and password in the body
-      const response = await axios.post('http://localhost:3001/login', {
-        username,
-        password
-      }, {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        
-      });
-      //check if fetch was successful 
-      if (response.status === 200) {
-        // const data = await response.json();
-        // setToken(data.token)
 
-        //extracting the token from the response and setting the token and isLoggedIn state variable
-        const { token } = response.data;
-        console.log(response.data)
-        setToken(token);          
-        setIsLoggedIn(true);
-        //navigate('/dashboard');
-       
-      } else {
-        alert("Login failed!");
-      }
-    } catch (error) {
-      alert('An error occurred while logging in.');
-    }
+  const handleLoginSuccess = (token) => {
+    setToken(token);
+    setIsLoggedIn(true);
   }
-
-
   return (
     <Router>
       <>
         <div>
           <h1>Workout Tracker</h1>
-          {!isLoggedIn ? ( 
-            <div>
-              <input
-                type='text'
-                placeholder='Username'
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-              <input
-                type='password'
-                placeholder='Password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <button onClick={handleLogin}>Log In</button>
-              <p>Not a user? Click below</p>
-              <Link to="/register">Register</Link>
-            </div>
+          {!isLoggedIn ? (
+            <Login onLoginSuccess={handleLoginSuccess} />
           ) : null}
         </div>
         <Routes>
           <Route path="/" element={<div>Home Page</div>} />
           <Route path="/users" element={<div>Users Page</div>} />
-          <Route path="/register" element={<Register />} />  
-          <Route path="/dashboard" element={<Dashboard />} />    
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </>
     </Router>
--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
-  return (
+    const [username, setUsername]= useState('');
+    const [password, setPassword] = useState('');
+    const [token, setToken] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
+    const handleLogin = async () => {
+        //try logging in
+        try {
+          //send post request to /login route with the username and password in the body
+          const response = await axios.post('http://localhost:3001/login', {
+            username,
+            password
+          }, {
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            
+          });
+          //check if fetch was successful 
+          if (response.status === 200) {
+            // const data = await response.json();
+            // setToken(data.token)
+    
+            //extracting the token from the response and setting the token and isLoggedIn state variable
+            const { token } = response.data;
+            console.log(response.data)
+            setToken(token);          
+            setIsLoggedIn(true);
+            navigate('/dashboard');
+            setUsername('');
+            setPassword('');
+           
+          } else {
+            alert("Login failed!");
+          }
+        } catch (error) {
+          alert('An error occurred while logging in.');
+        }
+      }
+      const handleLoginSuccess = (token) => {
+        setToken(token);
+        setIsLoggedIn(true);
+      }
+     
+    return (
     <div>
-      
+        <input
+        type='text'
+        placeholder='Username'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type='password'
+        placeholder='Password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Log In</button>
+      <p>Not a user? Click below</p>
+        <Link to="/register">Register</Link>
     </div>
   )
 }
