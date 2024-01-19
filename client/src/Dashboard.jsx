07e3559f9a8a8f4b2b3e80192c5fa4a366eadbae
lion48z <140863653+lion48z@users.noreply.github.com>
2024-01-18 21:52:19 -0600

    not broken but not working

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-//import DashboardForm from './DashboardForm';
-import './Dashboard.css';
-//import { Grid, Paper, Typography } from '@mui/material';
-
+import  './DashboardStyle.css'
+import DashboardForm from './DashboardForm'
 const Dashboard = () => {
-  //const [token, setToken] = useState('');
-  //const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [dashboardData, setDashboardData] = useState({
     activities: [],
     totalRunDistance: '',
@@ -16,11 +14,10 @@ const Dashboard = () => {
   useEffect(() => {
     // Retrieve the token from local storage
     const storedToken = localStorage.getItem('authToken');
-    console.log('Stored Token:', storedToken);
+
     if (storedToken) {
-     // setToken(storedToken);
-     // setIsLoggedIn(true);
-      console.log('About to fetch dashboard data');
+      setToken(storedToken);
+      setIsLoggedIn(true);
       // Fetch dashboard data using the stored token
       getDashboard(storedToken);
     } else {
@@ -28,36 +25,41 @@ const Dashboard = () => {
       console.log('User is not logged in');
     }
   }, []);
-
-  const getDashboard = async (storedToken) => {
+  const getDashboard = async () => {
+   
     try {
-      console.log('Making request with token:', storedToken);
-      const response = await axios.get('http://localhost:3001/dashboard', {
-        headers: {
-          'Authorization': `Bearer ${storedToken}`
+      if (isLoggedIn) {
+        const response = await axios.get('http://localhost:3001/dashboard', {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        console.log('Full API Response:', response);
+        if (response.status === 200) {
+          
+          console.log('Dashboard Data:', response.data);
+          setDashboardData(response.data);
         }
-      });
-      console.log('Full API Response:', response);
-
-      if (response.status === 200) {
-        console.log('Dashboard Data:', response.data);
-        setDashboardData(response.data);
+      } else {
+        // Handle the case when the user is not logged in
+        console.log('User is not logged in');
       }
     } catch (error) {
       alert('Error retrieving dashboard data', error);
     }
   };
-
   const { activities, totalRunDistance, totalWalkDistance, totalBikeDistance } = dashboardData;
- 
+  useEffect(() => {
+   
+    getDashboard();
+  }, [token, isLoggedIn]); 
   if (!activities || activities.length === 0) {
     return <div>Loading...</div>; // or render a loading state or an error message
   }
 
-
   return (
     <div>
-    {/*<DashboardForm  />*/}
+   <DashboardForm  />
 
     <div className="dashboard-grid">
       <div className="dashboard-item">
